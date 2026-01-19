73488595dfb921562046ca20a990e7e64fc03e37
Huynhtai <huynhkimtai101@gmail.com>
2026-01-19 20:55:07 +0700

    Hoàn thành API lấy danh sách sản phẩm theo loại có phân trang, limit và sắp xếp

--- src/services/productService.js
+++ src/services/productService.js
@@ -70,5 +70,68 @@ const getProductById = async (id) => {
     };
   }
 };
-
-export default { getAllProducts, getProductById };
+const getProductByCategories = async (id) => {
+  try {
+    const products = await db.Product.findAll({
+      include: [
+        {
+          model: db.Category,
+          where: { category_id: id },
+          through: { attributes: [] }
+        }
+      ],
+      limit: 10,
+      order: [["created_at", "DESC"]],
+    });
+    if (!products) {
+      return {
+        EM: "product not found",
+        EC: "0",
+        DT: [],
+      };
+    }
+    return {
+      EM: "Get product by categories success",
+      EC: "0",
+      DT: products,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      EM: "error from service",
+      EC: "-1",
+      DT: "",
+    };
+  }
+};
+const getProductByCategoriesWithPaginate = async (page, limit) => {
+    try {
+      let offset = (page - 1) * limit; 
+     
+      const { rows, count } = await db.Product.findAndCountAll({
+        include: [
+         {
+           model: db.Category
+         }
+        ],
+        limit: limit,
+        offset: offset,
+      })
+      return {
+        EM: "Get all product successfully",
+        EC: 0,
+        DT: {
+          totalRows: count,  //tổng số sản phẩm hiện tại trong db
+          totalPages: Math.ceil(count / limit),  //tính toán số nút trang 
+          product: rows,    //toàn bộ sản phẩm
+        },
+      };
+    } catch (error) {
+      return{
+        EM: "error from service",
+        EC: "-1",
+        DT: "",
+      }
+    }
+}
+export default { getAllProducts, getProductById, getProductByCategories ,getProductByCategoriesWithPaginate};
