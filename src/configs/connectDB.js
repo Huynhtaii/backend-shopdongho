e9e8107feb7cf62ae7c4aee1f43673b126b4c0d1
Huynhtaii <huynhkimtai101@gmail.com>
2026-01-30 21:53:47 +0700

    Update connectDB to use environment variables

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,16 +1,24 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('testJWT', 'root', null, {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-const Connection =async ()=>{
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
+  }
+);
+
+const Connection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 }
 export default Connection
